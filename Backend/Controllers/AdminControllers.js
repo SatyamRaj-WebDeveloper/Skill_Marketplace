d36a3bbe9bdd396a6ae7d24e80b14a56f8076479
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2025-09-27 09:29:25 +0530

    Some major changes to the Admin Controllers

--- Backend/Controllers/AdminControllers.js
+++ Backend/Controllers/AdminControllers.js
@@ -48,11 +48,29 @@ const cancelRequest = async(req,res) => {
 
 const getAllUsers = async(req , res)=>{
     try {
-        const users = await User.find();
+
+        const page = parseInt(req.query.page);
+        const limit  = parseInt(req.query.limit);
+        const skip = (page-1)*limit;
+        const users = await User.find()
+        .select('-password')
+        .skip(skip)
+        .limit(limit);
+         
+        const totalUsers = await User.countDocuments();
+
         if(!users){
             return res.status(404).json({message:"No users Found"});
         }
-        return res.status(200).json({message:"Users Fetched Successfully", data:users});
+        return res.status(200).json({
+            message:"Users Fetched Successfully",
+            data : users,
+            pagination : {
+                total: totalUsers,
+                page: page,
+                pages: Math.ceil(totalUsers / limit)
+            }
+        });
     } catch (error) {
         console.log(error.message);
         return res.status(500).json({message:"Internal Server Error"});
@@ -66,9 +84,13 @@ const suspendUser = async(req,res)=>{
         if(!user){
             return res.status(404).json({message:"No user found"});
         }
-        user.isActive = false;
-        await user.save();
-        return res.status(200).json({message:"User Suspended Succeffuly"});
+        if(user.id !== req.user.id){
+            user.isActive = false;
+            await user.save();
+            return res.status(200).json({message:"User Suspended Succeffuly"});
+        }else{
+            return res.status(400).json({message:"Admin Cannot Suspend their own Account"});
+        }
     } catch (error) {
         console.log(error.message);
         return res.status(500).json({message:"Internal Server Error"});
